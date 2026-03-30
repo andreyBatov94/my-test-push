692baf9d594af933bb2f3cf658002b908e23c4e8
Andrey Batov <134310246+andreyBatov94@users.noreply.github.com>
2026-03-30 16:46:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
+self.addEventListener('install', (event) => {
+    // Пропускаем ожидание и активируем новый воркер сразу
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    // Заставляем новый воркер сразу взять управление страницами
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('push', function(event) {
     const data = event.data ? event.data.json() : { title: 'Тест', body: 'Пустое сообщение' };
     
